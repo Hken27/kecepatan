4cea4eb4b0781f69f900b24ab6b549206aba716a
mbolang <105188581+Hken27@users.noreply.github.com>
2023-03-17 13:12:58 +0700

    Add files via upload

---
+++ Modul2/Menghitung.py
@@ -0,0 +1,11 @@
+print("\tTADIKA MESRA\n")
+
+def belajar_Menghitung():
+    print("Belajar Menghitung!!!!")
+       
+jarak = int (input("Masukkan jarak:"))
+waktu = int (input("Masukkan waktu:"))
+kecepatan = jarak * waktu
+print("Hasil adalah " , kecepatan)
+
+belajar_Menghitung()
